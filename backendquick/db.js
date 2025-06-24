0eb1acd256b15f030e0c17f2049b4d8e146e3b18
Bhuvanesh5-03 <santharajendran7@gmail.com>
2025-06-24 05:55:09 +0530

    Update CORS to allow Netlify frontend

--- backendquick/db.js
+++ backendquick/db.js
@@ -5,7 +5,6 @@ dotenv.config();
 export default async function getDataBase() {
     const client = await MongoClient.connect(process.env.MONGO_URI);
     const database = client.db('quickscore');
-    console.log("uri:",process.env.MONGO_URI)
     if (!database)
         console.log("Database not connected");
     return database;
